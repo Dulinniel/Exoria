4f14cc70eaa4e170c127f6b032e9515ac97ca718
Dulinniel <nightingalenervous@gmail.com>
2024-02-27 23:49:51 +0100

    End and optimization and link to inventory

--- Loot/weapon.bat
+++ Loot/weapon.bat
@@ -1,20 +1,21 @@
 @echo off
 setlocal EnableDelayedExpansion
 
+SET _inventoryManager=%cd%\Inventory\manager.bat
 SET _weaponFolder=%cd%\Weapons
 SET _pickupFolder=%_weaponFolder%\%1\%2
-SET _count=0
-SET _index=0
+SET /a _count=0
+SET /a _index=0
 SET _chosen=
 
 REM put all the files into a pseudo-array prefixed with "WEAPON_"
 FOR /r %_pickupFolder% %%A IN (*.bat) DO (
-  SET WEAPON_!_count!=%%~A
+  SET WEAPON_!_count!=%%~A 
   SET /a _count+=1
 )
 
 SET /a _index=%random% %% %_count%
 CALL !WEAPON_%_index%! name _chosen
-REM Use _chosen variable to update it here and being able to pass it as reference in the calling program
-ECHO %_chosen% >> NUL
+REM Add the weapon to the inventory
+CALL %_inventoryManager% add "%_chosen%" 1 !WEAPON_%_index%!
 endlocal & SET %~3=%_chosen%
