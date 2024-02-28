12d7a7ac68f8ed60e9d205e280133a4d66bb0eb4
Dulinniel <nightingalenervous@gmail.com>
2024-02-28 17:31:19 +0100

    Fix and improve the add function in the inventory manager

--- Inventory/manager.bat
+++ Inventory/manager.bat
@@ -1,32 +1,31 @@
-@echo off
+@ECHO off
 
 SET _count=0
-SET _found=
+SET _tempFileLocation=%_inventoryFolderLocation%\temp-inventory.txt
 SET _storageFile=
-SET out="new_equipement.txt"
+SET /a _empty=1
 
 GOTO %1
 
 :add
+COPY NUL %_tempFileLocation% > NUL
 FOR /f %%G IN ( %_equipementFileLocation% ) DO SET /a _count+=1
-IF %_count% GTR 20 (
-	 SET _storageFile=%_bagInventoryFileLocation% 
+IF %_count% GEQ 20 (
+	SET _storageFile=%_bagInventoryFileLocation% 
 ) ELSE (
 	SET _storageFile=%_equipementFileLocation%
 )
-
-SET /a _count=0
-
-FOR /f "tokens=1,2 delims=-" %%G IN (%_storageFile%) DO (
-  SET /a _count+=1
-  IF %%G ==%2 ( 
-    IF %%H GTR 10 (
-      ECHO %2-%3-%4 >> %out%
-    ) ELSE (
-      SET _x=%%H
-      SET /a %_x%+=1
-      ECHO %2-%_x%-%4 >> %out%
-    )
-  )
+setlocal EnableDelayedExpansion
+FOR /f "tokens=1,2,3 delims=-" %%G IN (%_storageFile%) DO (
+  SET /a _empty=0
+  IF %%G==%2 ( 
+    SET /a _new_quantity=%%H+%3
+    ECHO %%G-!_new_quantity!-%%I >> %_tempFileLocation%
+  ) ELSE ECHO %%G-1-%%I >> %_tempFileLocation%
 )
+IF %_empty%==1 ECHO %2-%3-%4 >> %_tempFileLocation%
+endlocal
+
+TYPE %_tempFileLocation% > %_storageFile%
+DEL %_tempFileLocation%
 
